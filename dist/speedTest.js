30f1f2bbde5087029fb408ff050e2bf3cf00c9f3
humbl3man <imhumbl3@gmail.com>
2016-03-13 21:02:28 -0700

    refactor code to use closure

--- dist/speedTest.js
+++ dist/speedTest.js
@@ -1,24 +1,19 @@
-var SpeedTest = function(testFunc, repetitions) {
-  this.testFunc = testFunc;
-  this.repetitions = repetitions || 10000;
-  this.average = 0;
-};
+var SPEEDTEST = function (testFn, repetitions) {
+  var fn = testFn,
+      reps = repetitions || 10000,
+      average = 0;
+  return {
+    startTest: function () {
+      var beginTime, endTime, sumTimes = 0;
 
-SpeedTest.prototype = {
-  startTest: function () {
-    var beginTime, endTime, sumTimes = 0;
-
-    for (var i = 0, x = this.repetitions; i < x; i += 1) {
-      beginTime = + new Date();
-      this.testFunc();
-      endTime = + new Date();
-      sumTimes += endTime - beginTime;
+      for (var i = 0, x = reps; i < x; i += 1) {
+        beginTime = + new Date();
+        fn();
+        endTime = + new Date();
+        sumTimes += endTime - beginTime;
+      }
+      average = sumTimes / reps;
+      return 'Speed for repeating ' + reps + ' times: ' + average;
     }
-    this.average = sumTimes / this.repetitions;
-    return 'Speed for repeating ' + this.repetitions + ' times: ' + this.average;
-  }
+  };
 };
-
-$(function () {
-  'use strict';
-});
